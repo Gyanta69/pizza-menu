4ac7cd2fe47047d1f60473d76e56fccb176275e9
Pragyan Khadka <137318099+Gyanta69@users.noreply.github.com>
2025-07-04 10:57:36 +0545

    Updated menu of index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -73,11 +73,30 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza name='Pizza Spinaci' ingredients = "Tomato, mozarella, spinach, and ricotta cheese" 
+      photoName = "pizzas/spinaci.jpg" price = {10}/>
+
+      <Pizza name="Pizza Funghi" ingredients="Tomato, mozarella, mushrooms, and onion"
+      photoName="pizzas/funghi.jpg" price= {12}/>
+
     </main>
+  );
+}
+
+function Pizza( props) {
+  console.log(props);
+
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+
+      </div>
+
+    </div>
   );
 }
 
@@ -96,15 +115,6 @@ function Footer() {
   );
 }
 
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spanchi" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
