9fa7dd35837718d44f6343c405c5c3f4439efc6b
Pragyan Khadka <137318099+Gyanta69@users.noreply.github.com>
2025-07-04 10:45:05 +0545

    Update css styling of  index.js

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,18 +58,26 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  //const style ={ color: "red", fontSize: "50px", textTransform: "uppercase" };
+  return (
+  <header className="header"> 
+    <h1>
+    Fast React Pizza Co.
+    </h1>
+  </header>
+
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
-    </div>
+    </main>
   );
 }
 
@@ -83,7 +92,7 @@ function Footer() {
   // else alert("Sorry, we are closed");
 
   return (
-    <footer>{new Date().toLocaleDateString()}We are currently open</footer>
+    <footer className="footer">{new Date().toLocaleDateString()}We are currently open</footer>
   );
 }
 
@@ -91,7 +100,7 @@ function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza Spanchi" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
